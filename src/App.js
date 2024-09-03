c4a9954fce8cb6c7034ea0c8cbc6ba05acad7f26
bigexperiment <ganesha.727@gmail.com>
2024-09-03 12:08:07 -0700

    d

--- src/App.js
+++ src/App.js
@@ -123,13 +123,14 @@ const SkyBuddySimple = () => {
             <SignedOut>
               <button
                 onClick={() => setShowSignIn(true)}
-                className="bg-white text-teal-600 py-2 px-4 rounded-md hover:bg-teal-100 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 transition duration-300"
+                className=" ok bg-white text-teal-600 py-2 px-4 rounded-md hover:bg-teal-100 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 transition duration-300"
               >
                 Sign In
               </button>
             </SignedOut>
           </nav>
         </div>
+        
       </header>
 
       <main className="flex-grow container mx-auto py-8 px-4">
