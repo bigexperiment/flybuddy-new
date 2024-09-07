9f7c1a8f48754c9c39ba35e24daedc8de6d1478f
bigexperiment <ganesha.727@gmail.com>
2024-09-07 11:22:38 -0700

    3e4

--- src/App.js
+++ src/App.js
@@ -402,6 +402,10 @@ const SkyMatesSimple = () => {
                                 <MapPin className="mr-2" size={16} />
                                 To: {passenger.tocity || "N/A"}
                               </p>
+                              <p className="text-gray-600 flex items-center">
+                                <PlaneTakeoff className="mr-2" size={16} />
+                                Airlines: {passenger.airlines || "N/A"}
+                              </p>
                               <button
                                 onClick={() => handleContact(passenger.phone)}
                                 className="mt-4 bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition duration-300 flex items-center justify-center w-full"
